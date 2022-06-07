77e95adf8840aea51a890196644d3aff7ad73add
Oskari Niskanen <oskari.niskanen97@gmail.com>
2022-06-07 13:24:51 +0200

    Added account manager server app.

---
+++ manager-app/app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var logger = require('morgan');
+http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+var transactionRouter = require('./routes/transactionRouter');
+var accountRouter = require('./routes/accountRouter');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/transactions', transactionRouter);
+app.use('/accounts', accountRouter);
+
+
+// Catch 404 and forward it
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+  
+
+// Error handler
+app.use(function(err, req, res, next) {
+    // Only provide error in dev
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Account manager running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
---
+++ manager-app/routes/transactionRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const transactionRouter = express.Router();
+transactionRouter.use(bodyParser.json());
+
+// Storing account numbers and balances in memory
+var accounts = {}
+
+transactionRouter.route('/')
+.get((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(accounts);
+});
+
+transactionRouter.route('/:accountNumber')
+.get((req, res, next) => {
+    res.statusCode = 403;
+    res.end('GET operation not supported on /transaction/'+ req.params.accountNumber);
+})
+.post((req, res, next) => {
+    if (req.body.amount != null) {
+        // Check if account has previous entries
+        if (accounts[req.params.accountNumber] == null){
+            // Create an entry for the given bank account and set initial balance as zero.
+            accounts[req.params.accountNumber] = {"balance": 0.0, "transactions": []}
+        }
+
+        var transAmount = parseFloat(req.body.amount);
+        var timestamp = new Date().getTime();
+        // Calculate balance after transaction
+        let newBalance = (accounts[req.params.accountNumber]["balance"] +
+                         transAmount).toFixed(2);
+        // Updating the balance.
+        accounts[req.params.accountNumber]["balance"] = parseFloat(newBalance)
+        // Updating transaction info and timestamp.
+        accounts[req.params.accountNumber]["transactions"].push({"amount": transAmount, "timestamp": timestamp })
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        // Returning account data as response
+        let response = accounts[req.params.accountNumber];
+        res.json(response)
+    } else {
+        res.statusCode = 400;
+        res.end("Transaction amount missing.")
+    }
+})
+
+module.exports = transactionRouter;
