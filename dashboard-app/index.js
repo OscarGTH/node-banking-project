e62179776a80f7e24262c005758e8cdb909c10a9
Oskari Niskanen <oskari.niskanen97@gmail.com>
2022-06-17 16:47:47 +0200

    Set up a proxy middleware so requests from browser get proxied to backend server.

--- dashboard-app/index.js
+++ dashboard-app/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var { createProxyMiddleware } = require('http-proxy-middleware');
 var indexRouter = require('./routes/index');
-var port = 8080
-var hostname = '0.0.0.0'
+var port = process.env.PORT || 8080
+var hostname = process.env.HOST || '0.0.0.0'
+const PROXY_HOST = process.env.PROXY_HOST || '0.0.0.0'
+const PROXY_PORT = process.env.PROXY_PORT || 8081;
 
 var app = express();
 
 app.use(express.static('public'))
 app.use('/', indexRouter);
+app.use('/transactions', createProxyMiddleware({target: 'http://' + PROXY_HOST + ':' + PROXY_PORT, changeOrigin: true}));
 
 app.listen(port, hostname);
 console.log(`Server runnning on http://${hostname}:${port}`);
--- dashboard-app/routes/index.js
+++ dashboard-app/routes/index.js
@@ -1,39 +1,9 @@
 var express = require('express');
-var axios = require('axios');
 var router = express.Router();
-const PORT = process.env.PORT || '8081';
-const HOST = process.env.HOST || '0.0.0.0';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home Page' });
-});
-
-router.get('/transactions', function(req, res, next) {
-  // Getting transaction data from server
-  axios.get('http://' + HOST + ':' + PORT + '/transactions')
-  .then(function (response) {
-    console.log(response)
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(response.data);
-  })
-  .catch(function (error) {
-      console.log(error.message);
-  });
-});
-
-router.get('/transactions/:accountNumber', function(req, res, next) {
-  // Getting transaction data from server
-  axios.get('http://' + HOST + ':' + PORT + '/transactions/' + accountNumber)
-  .then(function (response) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(response);
-  })
-  .catch(function (error) {
-      console.log(error.message);
-  });
 });
 
 module.exports = router;
